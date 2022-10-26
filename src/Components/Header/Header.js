9e2c9e125f289bb96bd584bdc80fe3e48f3d940c
Rajendra <itsrajendrapatel@gmail.com>
2022-10-26 11:32:07 +0530

    signout page designed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import {Menu, Search, Apps, Notifications, VideoCall} from "@mui/icons-material"
-import {Button, Avatar} from "@mui/material"
+import React,{useState} from 'react'
+import {Menu, Search, Apps, Notifications, VideoCall, PersonAddOutlined} from "@mui/icons-material"
+import {Button, Avatar, Popover} from "@mui/material"
 import logo from "../../assets/logo1.png"
 import "./Styles.css"
 
+
+
 const Header = () => {
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) =>{
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () =>{
+      setAnchorEl(null)
+  }
+
+  const open  = Boolean(anchorEl)
+  const id = open ? "simple-popover" : undefined;
+
   return (
     <div className='header'>
         <div className="header_left">
@@ -21,7 +35,48 @@ const Header = () => {
             <VideoCall/>
             <Apps/>
             <Notifications />
-            <Avatar />
+            <Avatar onClick={handleClick} />
+            {/* POP PART OF THE WEBSITE  */}
+            <Popover open={open} id={id} onClose={handleClose} anchorEl={anchorEl}   anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}>
+              {/* MAIN HTML OF THE POP OVER  */}
+              <div className="home_popoverContainer">
+                <div className="home_popover_top">
+                    <Avatar className='header_avatar' alt="Remy Sharp"
+  src="/static/images/avatar/1.jpg"
+  sx={{ width: 70, height: 70 }} />
+                  {/* Main part of the Popout Html  */}
+                  <div className="home_text">
+                    <div className="home_displayName">
+                      Rajendra Patel
+                    </div>
+                    <div className="home_mail">
+                        rajendraPatelofficial@gmail.com
+                    </div>
+                  </div>
+                  {/* second part */}
+                </div>
+                <div className="home_popover_btm">
+                  <div className="home_addBtn">
+                    <PersonAddOutlined className='home_addIcon' />
+                  </div>
+                  <Button variant='outlined' className='home_signOut'>
+                    Sign Out
+                  </Button>
+                  <div className="home_popover_footer">
+                    <p>Privacy Policy</p>
+                    <span>.</span>
+                    <p>Terms of service</p>
+                  </div>
+                </div>
+              </div>
+            </Popover>
         </div>
     </div>
   )
--- src/Components/RightSideVideos/RightSide.jsx
+++ src/Components/RightSideVideos/RightSide.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./RightSide.css";
+import { useNavigate } from "react-router-dom";
 
 const RightSide = () => {
+  const Navigaters = useNavigate();
+  const Imgredirect = () => {
+    Navigaters("/watch");
+  };
+
   return (
     <div className="RightSide">
       <div className="Rightside_left">
         <img
+          onClick={Imgredirect}
           src="https://images.livemint.com/img/2022/10/22/600x338/LVM3-MII_1666469421488_1666469808603_1666469808603.jpg"
           alt="Rocket"
           className="RightSide_thumbnail"
