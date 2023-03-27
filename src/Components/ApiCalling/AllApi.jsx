9bcc567440768f9d5a5095bd525bf9820b7b4d7e
Rajendra Patel <itsrajendrapatel@gmail.com>
2023-03-27 10:12:04 +0530

    Update AllApi.jsx

--- src/Components/ApiCalling/AllApi.jsx
+++ src/Components/ApiCalling/AllApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const FetchingData = () => {
-  return axios.get`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&key=AIzaSyB9XbyPZGtnp09wESePbsQ2lAvrUEMM_t4&maxResults=50&regionCode=IN`;
+  return axios.get`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&key=AIzaSyBWuaDNXvXZisZwd5oSqrY4fJQvLM2T05k&maxResults=40&regionCode=IN`;
 };
 
 export const SettingVideoData = () => {};
